e18d6d2f2b2fdecf5a6bb4b6caf990707f460d05
Perry Xie <perry.xie1@gmail.com>
2022-02-07 13:39:26 -0800

    Work on index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Root from './components/root';
+import Root from './components/Root';
 import configureStore from './store/store';
 import jwt_decode from 'jwt-decode';
 import { setAuthToken } from './util/session_api_util';
