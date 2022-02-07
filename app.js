0c6cd8582000c9afd2d24b716b1d2957c2622530
JJZhang <92764807+jjzhang329@users.noreply.github.com>
2022-02-07 15:34:26 -0600

    backend user-auth

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const passport = require('passport');
+
+
 
 mongoose
     .connect(db, { useNewUrlParser: true })
@@ -12,9 +16,11 @@ mongoose
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use("/api/users", users);
+app.use(passport.initialize())
+require('./config/passport')(passport);
 
 
-app.get("/", (req, res) => res.send("Hello World"));
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
---
+++ validation/register.js
@@ -0,0 +1,57 @@
+const Validator = require('validator');
+const validText = require('./valid-text');
+
+module.exports = function validateRegisterInput(data) {
+    let errors = {};
+
+    data.username = validText(data.username) ? data.username : '';
+    data.email = validText(data.email) ? data.email : '';
+    data.password = validText(data.password) ? data.password : '';
+    data.password2 = validText(data.password2) ? data.password2 : '';
+    data.role = validText(data.role) ? data.role : "";
+    data.languages = validText(data.languages) ? data.languages : [];
+
+    if(Validator.isEmpty(data.role)){
+        errors.role = 'pls choose your role as a student or tutor'
+    }
+    if (Validator.isEmpty(data.languages)){
+        errors.languages = 'pls select at least one language'
+    }
+
+    if (!Validator.isLength(data.username, { min: 2, max: 30 })) {
+        errors.username = 'username must be between 2 and 30 characters';
+    }
+
+    if (Validator.isEmpty(data.username)) {
+        errors.username = 'username field is required';
+    }
+
+    if (Validator.isEmpty(data.email)) {
+        errors.email = 'Email field is required';
+    }
+
+    if (!Validator.isEmail(data.email)) {
+        errors.email = 'Email is invalid';
+    }
+
+    if (Validator.isEmpty(data.password)) {
+        errors.password = 'Password field is required';
+    }
+
+    if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+        errors.password = 'Password must be at least 6 characters';
+    }
+
+    if (Validator.isEmpty(data.password2)) {
+        errors.password2 = 'Confirm Password field is required';
+    }
+
+    if (!Validator.equals(data.password, data.password2)) {
+        errors.password2 = 'Passwords must match';
+    }
+
+    return {
+        errors,
+        isValid: Object.keys(errors).length === 0
+    };
+};
