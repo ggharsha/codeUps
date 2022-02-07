76c2b26ba3543943bcbc9fa7645b2597a83d224b
harsha <ggharshabandi@gmail.com>
2022-02-07 16:17:07 -0600

    Added socket.io backend

--- app.js
+++ app.js
@@ -6,7 +6,16 @@ const db = require('./config/keys').mongoURI;
 const users = require("./routes/api/users");
 const passport = require('passport');
 
-
+// socket.io
+const http = require("http");
+const server = http.createServer(app);
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+});
+// end socket.io
 
 mongoose
     .connect(db, { useNewUrlParser: true })
@@ -16,11 +25,32 @@ mongoose
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 app.use("/api/users", users);
+
 app.use(passport.initialize())
 require('./config/passport')(passport);
-
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+// socket.io
+io.on("connection", (socket) => {
+    socket.emit("me", socket.id)
+
+    socket.on("disconnect", () => {
+        socket.broadcast.emit("callEnded")
+    })
+
+    socket.on("callUser", data => {
+        io.to(data.userToCall).emit("callUser", { 
+            signal: data.signalData, from: data.from, name: data.name 
+        })
+    })
+
+    socket.on("answerCall", data => {
+        io.to(data.to).emit("callAccepted", data.signal)
+    })
+})
+// end socket.io
