e9e8797239b6c312d32b1151fffb1576abc77a5a
JJZhang <92764807+jjzhang329@users.noreply.github.com>
2022-02-07 12:28:10 -0600

    setting up mongoose

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
-// const users = require("./routes/api/users");
+const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI;
+
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch(err => console.log(err));
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
