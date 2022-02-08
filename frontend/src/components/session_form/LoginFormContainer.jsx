ace7a11bd2a42f686bd70c5a9ce7274eb964b6c3
Perry Xie <perry.xie1@gmail.com>
2022-02-07 16:41:49 -0800

    Add login form

---
+++ frontend/src/components/session_form/LoginFormContainer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { login } from '../../actions/session_actions';
+import LoginForm from './LoginForm';
+
+const mapStateToProps = (state) => {
+  return {
+    errors: state.errors.session
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    login: (user) => dispatch(login(user))
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(LoginForm);
