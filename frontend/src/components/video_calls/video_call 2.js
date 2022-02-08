78853c3727947f95d43b706fd3907a75ee4e86c3
harsha <ggharshabandi@gmail.com>
2022-02-07 18:29:16 -0600

    Added useEffect for video call

--- frontend/src/components/video_calls/video_call 2.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class VideoCall extends React.Component {
-
-
-    render() {
-        
-    }
-}
--- frontend/src/components/video_calls/video_call.js
+++ frontend/src/components/video_calls/video_call.js
@@ -12,5 +12,32 @@ function VideoCall() {
     const [ receivingCall, setReceivingCall ] = useState(false);
     const [ caller, setCaller ] = useState("");
     const [ callerSignal, setCallerSignal ] = useState();
+    const [ callAccepted, setCallAccepted ] = useState(false);
+    const [ idToCall, setIdToCall ] = useState("");
+    const [ callEnded, setCallEnded ] = useState(false);
+    const [ name, setName ] = useState("");
+    const myVideo = useRef();
+    const userVideo = useRef();
+    const connectionRef = useRef();
+
+    useEffect(() => {
+        navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+        .then(stream => {
+            setStream(stream)
+            myVideo.current.srcObject = stream
+        })
+
+        socket.on("me", id => {
+            setMe(id)
+        })
+
+        socket.on("callUser", data => {
+            setReceivingCall(true);
+            setCaller(data.from);
+            setName(data.name);
+            setCallerSignal(data.signal);
+        })
+    }, [])
+
     
 }
