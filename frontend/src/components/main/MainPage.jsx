0bab591fae878f18b99611871b5a8430f145eac1
Michelle Huang <michelleahuang@berkeley.edu>
2022-02-08 11:27:15 -0800

    Working on splash CSS

--- frontend/src/components/main/MainPage.jsx
+++ frontend/src/components/main/MainPage.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class MainPage extends React.Component {
 
   render() {
     return (
       <div className="main-page-container">
+        <div className="main-page-left">
+          <div className="main-page-tagline">
+            
+          </div>
+          <Link className="session-buttons joinus" to={'/signup'}>Join Us</Link>
+        </div>
+        <div className="main-page-right">
+
+        </div>
+
         <h1>A Twitter Clone</h1>
         <footer>
           Copyright &copy; 2019 Chirper
