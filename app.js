64fc813a242652ae919585770235ed33d496b662
harsha <ggharshabandi@gmail.com>
2022-02-08 14:02:21 -0600

    Refactored app.js so server is running in server.js

--- app.js
+++ app.js
@@ -16,23 +16,6 @@ if (process.env.NODE_ENV === 'production') {
     })
 }
 
-// socket.io
-const http = require("http");
-const server = http.createServer(app);
-const cors = require("cors");
-
-app.use(cors());
-
-const io = require("socket.io")(server, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"],
-        cors: true,
-        allowEI03: true
-    }
-});
-// end socket.io
-
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB successfully"))
@@ -46,32 +29,6 @@ app.use("/api/users", users);
 
 app.use(passport.initialize())
 require('./config/passport')(passport);
-
-
-
-// socket.io
-io.on("connection", (socket) => {
-    socket.emit("me", socket.id)
-    console.log("connected")
-
-    socket.on("disconnect", () => {
-        socket.broadcast.emit("callEnded")
-        console.log("disconnected")
-    })
-
-    socket.on("callUser", data => {
-        io.to(data.userToCall).emit("callUser", { 
-            signal: data.signalData, from: data.from, name: data.name 
-        })
-        console.log("call user")
-    })
-
-    socket.on("answerCall", data => {
-        io.to(data.to).emit("callAccepted", data.signal)
-        console.log("answer call")
-    })
-})
-// end socket.io
 
 //images
 // app.post('/images', upload.single('image')(req, res)=>{
@@ -87,4 +44,3 @@ app.get('/images/:key', (req, res)=>{
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
-server.listen(9000, () => console.log(`Server is running on port 9000`));
