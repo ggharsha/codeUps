499db20a919826170024b53ddd1ffb2dfef784f1
harsha <ggharshabandi@gmail.com>
2022-02-07 20:06:52 -0600

    Adding to render function for video calling

--- frontend/src/components/video_calls/video_call.js
+++ frontend/src/components/video_calls/video_call.js
@@ -102,6 +102,76 @@ function VideoCall() {
                     { callAccepted && !callEnded ? <video playsInline ref={userVideo} autoPlay style={{ width: "600px" }} /> : null }
                 </div>
             </div>
+
+            <div className='my-id'>
+                
+            </div>
+            {/* <div className="myId">
+
+                <TextField
+                    id="filled-basic"
+                    label="Name"
+                    variant="filled"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    style={{ marginBottom: "20px" }}
+                />
+
+                <CopyToClipboard 
+                    text={me} 
+                    style={{ marginBottom: "2rem" }}
+                >
+
+                    <Button 
+                    variant="contained" 
+                    color="primary" 
+                    startIcon={<AssignmentIcon fontSize="large" />}
+                    >
+                        Copy ID
+                    </Button>
+                </CopyToClipboard>
+
+                <TextField
+                    id="filled-basic"
+                    label="ID to call"
+                    variant="filled"
+                    value={idToCall}
+                    onChange={(e) => setIdToCall(e.target.value)}
+                />
+
+                <div className="call-button">
+
+                    {callAccepted && !callEnded ? (
+                        <Button 
+                            variant="contained" 
+                            color="secondary" 
+                            onClick={leaveCall}
+                        >
+                            End Call
+                        </Button>
+                    ) : (
+                        <IconButton 
+                            color="primary" 
+                            aria-label="call" 
+                            onClick={() => callUser(idToCall)}
+                        >
+                            Phone Icon
+                            <PhoneIcon fontSize="large" />
+                        </IconButton>
+                    )}
+                    {idToCall}
+                </div>
+            </div>
+            <div>
+                {receivingCall && !callAccepted ? (
+                    <div className="caller">
+                        <h1 >{name} is calling...</h1>
+                        <Button variant="contained" color="primary" onClick={answerCall}>
+                            Answer
+                        </Button>
+                    </div>
+                ) : null}
+            </div> */}
         </div>
     )
 }
