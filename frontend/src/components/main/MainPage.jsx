a8a6e2fbf4a6bf921a633616cfde08d577f46d3c
Michelle Huang <michelleahuang@berkeley.edu>
2022-02-08 14:17:05 -0800

    Typewriter works

--- frontend/src/components/main/MainPage.jsx
+++ frontend/src/components/main/MainPage.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Typewriter from 'typewriter-effect';
+
 
 class MainPage extends React.Component {
+
 
   render() {
     return (
       <div className="main-page-container">
         <div className="main-page-left">
           <div className="main-page-tagline">
-            
+            <Typewriter className="typewriter"
+              options={{
+                strings: ["Grow", "Learn", "Understand", "Guide", "Excel"],
+                autoStart: true,
+                loop: true,
+              }}
+            />
           </div>
-          <Link className="session-buttons joinus" to={'/signup'}>Join Us</Link>
+          <p id="tagline-codeups">with codeUps</p>
+          <div>
+            <Link className="session-buttons joinus" to={'/signup'}>Join Us</Link>
+          </div>
         </div>
         <div className="main-page-right">
           <img className='splash-photo' src={require('../../images/splash.png')} />
