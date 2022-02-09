7c40e5b4350c5b200916da808107b13698de052e
Michelle Huang <michelleahuang@berkeley.edu>
2022-02-08 21:49:54 -0800

    Adding footer

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -9,17 +9,57 @@ class Footer extends React.Component {
                         DEVELOPERS
                     </div>
                     <div className="name-container">
-                        <div id="harsha">
-
+                        <div className="name-links">
+                            <p>HARSHA BANDI</p>
+                            <div className="prof-links">
+                                <a href="https://www.linkedin.com/in/harsha-bandi-57b260225/" target="_blank">
+                                    <i className="fa-brands fa-linkedin"></i>
+                                    <p>LinkedIn</p>
+                                </a>
+                                <a href="https://github.com/ggharsha" target="_blank">
+                                    <i className="fa-brands fa-github"></i>
+                                    <p>GitHub</p>
+                                </a>
+                            </div>
                         </div>
-                        <div id="jingjing">
-
+                        <div className="name-links">
+                            <p>JING JING ZHANG</p>
+                            <div className="prof-links">
+                                <a href="https://www.linkedin.com/in/jing-jing-zhang-888a05a5/" target="_blank">
+                                    <i className="fa-brands fa-linkedin"></i>
+                                    <p>LinkedIn</p>
+                                </a>
+                                <a href="https://github.com/jjzhang329" target="_blank">
+                                    <i className="fa-brands fa-github"></i>
+                                    <p>GitHub</p>
+                                </a>
+                            </div>
                         </div>
-                        <div id="michelle">
-
+                        <div className="name-links">
+                            <p>MICHELLE HUANG</p>
+                            <div className="prof-links">
+                                <a href="https://www.linkedin.com/in/michelleahuang/" target="_blank">
+                                    <i className="fa-brands fa-linkedin"></i>
+                                    <p>LinkedIn</p>
+                                </a>
+                                <a href="https://github.com/michelleahuang" target="_blank">
+                                    <i className="fa-brands fa-github"></i>
+                                    <p>GitHub</p>
+                                </a>
+                            </div>
                         </div>
-                        <div id="perry">
-
+                        <div className="name-links">
+                            <p>PERRY XIE</p>
+                            <div className="prof-links">
+                                <a href="https://www.linkedin.com/in/thepshay/" target="_blank">
+                                    <i className="fa-brands fa-linkedin"></i>
+                                    <p>LinkedIn</p>
+                                </a>
+                                <a href="https://github.com/thepshay" target="_blank">
+                                    <i className="fa-brands fa-github"></i>
+                                    <p>GitHub</p>
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
