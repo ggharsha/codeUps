9787b9de602fc7252760ec6b80c7414c089b0dbb
Michelle Huang <michelleahuang@berkeley.edu>
2022-02-08 20:06:28 -0800

    Fix styling

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -3,8 +3,26 @@ import React from 'react';
 class Footer extends React.Component {
     render() {
         return (
-            <div>
-                
+            <div className="footer-container">
+                <div className="developers">
+                    DEVELOPERS
+                </div>
+                <div>
+                </div>
+                <div className="name-container">
+                    <div id="harsha">
+
+                    </div>
+                    <div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
             </div>
         )
     }
