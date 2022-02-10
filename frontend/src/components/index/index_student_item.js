95a2d028d80c6295411422976268da7c87472828
harsha <ggharshabandi@gmail.com>
2022-02-09 19:04:25 -0600

    Adding index

--- frontend/src/components/index/index_student_item.js
+++ frontend/src/components/index/index_student_item.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 const IndexStudentItem = ({ student, fetchUser }) => (
     <li className="student-index-item">
+        <img className="index-item-image" src="#"/>
         <Link to={`/user/${student._id}`}>{student.username}</Link>
         <ul className="student-languages">
             {student.languages.map((lang, idx) => (
--- frontend/src/components/index/index_tutor_item.js
+++ frontend/src/components/index/index_tutor_item.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 const IndexTutorItem = ({ tutor, fetchUser }) => (
     <li className="tutor-index-item">
+        <img className="index-item-image" src="#" />
         <Link to={`/user/${tutor._id}`}>{tutor.username}</Link>
         <ul className="tutor-languages">
             {tutor.languages.map((lang, idx) => (
