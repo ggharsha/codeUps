f7478730ea275dd341c82c7bdc4418d8943ec19d
Michelle Huang <michelleahuang@berkeley.edu>
2022-02-09 16:42:35 -0800

    Working on reviews container

--- frontend/src/components/reviews/ReviewContainer.jsx
+++ frontend/src/components/reviews/ReviewContainer.jsx
@@ -1,15 +1,22 @@
 import { connect } from 'react-redux';
+import { getReviews, createReview, updateReview, deleteReview } from '../../actions/review_actions';
 import Reviews from './Reviews';
 
 const mapStateToProps = (state) => {
-    return {
+    const userId = ownProps.match.params.userId;
 
+    return {
+        userId: userId,
+        user: state.user.profileUser[userId]
     }
 }
 
 const mapDispatchToProps = (dispatch) => {
     return {
-
+        getReviews: (id) => dispatch(getReviews(id)),
+        createReview: (review) => dispatch(createReview(review)),
+        updateReview: (review) => dispatch(updateReview(review)),
+        deleteReview: (reviewId) => dispatch(deleteReview(reviewId))
     }
 }
 
