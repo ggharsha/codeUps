ed00df530fc57c4040b54764f84f17872f184a32
harsha <ggharshabandi@gmail.com>
2022-02-09 22:17:54 -0600

    Finished modal skeleton

--- frontend/src/components/user/UserPageContainer.jsx
+++ frontend/src/components/user/UserPageContainer.jsx
@@ -1,6 +1,7 @@
 import { connect } from 'react-redux';
 import { fetchUser } from '../../actions/user_actions';
 import UserPage from './UserPage';
+import { openModal } from '../../actions/modal_actions';
 
 const mapStateToProps = (state, ownProps) => {
   const userId = ownProps.match.params.userId;
@@ -13,7 +14,8 @@ const mapStateToProps = (state, ownProps) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    fetchUser: (userId) => dispatch(fetchUser(userId))
+    fetchUser: (userId) => dispatch(fetchUser(userId)),
+    openModal: modal => dispatch(openModal(modal))
   }
 }
 
