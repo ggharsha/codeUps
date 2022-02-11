5106e48fa51db1115061d70455fcbb9e4827115e
harsha <ggharshabandi@gmail.com>
2022-02-11 11:41:15 -0600

    Added AWS keys

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,5 +1,9 @@
 // keys_prod.js
 module.exports = {
     mongoURI: process.env.MONGO_URI,
-    secretOrKey: process.env.SECRET_OR_KEY
+    secretOrKey: process.env.SECRET_OR_KEY,
+    AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME,
+    AWS_BUCKET_REGION: process.env.AWS_BUCKET_REGION,
+    AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY,
+    AWS_SECRET_KEY: process.env.AWS_SECRET_KEY
 }
