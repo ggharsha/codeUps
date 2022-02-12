d17cf623adb619c2f2d72bb079b5490706ef589b
harsha <ggharshabandi@gmail.com>
2022-02-12 16:34:50 -0600

    Merging main

--- frontend/src/components/navbar/inbox/InboxModal.jsx
+++ frontend/src/components/navbar/inbox/InboxModal.jsx
@@ -1,1 +1,5 @@
 import React from "react";
+
+export default class InboxModal extends React.Component {
+    
+}
--- frontend/src/components/navbar/inbox/InboxModalContainer.jsx
+++ frontend/src/components/navbar/inbox/InboxModalContainer.jsx
@@ -1,1 +1,14 @@
 import { connect } from "react-redux";
+import { fetchAllInboxes, createInbox, updateInbox } from "../../../actions/inbox_actions";
+import InboxModal from "./InboxModal";
+
+const mSTP = state => ({
+    currentUserId: state.session.user.id,
+
+});
+
+const mDTP = dispatch => ({
+    fetchAllInboxes: userId => dispatch(fetchAllInboxes(userId))
+});
+
+export default connect(mSTP, mDTP)(InboxModal);
