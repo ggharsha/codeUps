9313709b6f0856ccbdf55e74a5522cced9397ba5
JJZhang <92764807+jjzhang329@users.noreply.github.com>
2022-02-12 15:44:08 -0600

    backend-inbox

---
+++ frontend/src/util/inbox_api_util.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+export const fetchAllMessages = (userId)=>{
+    return axios.get(`/api/inbox/`, userId)
+}
+
+export const createInbox = (inbox)=>{
+    return axios.post(`/api/inbox/new`, inbox)
+}
+
+export const updateInbox = (inbox) => {
+    return axios.patch(`/api/inbox/update`, inbox)
+}
