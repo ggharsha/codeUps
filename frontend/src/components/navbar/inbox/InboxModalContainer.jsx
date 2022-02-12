88cd7f332b2947d2a3fde236b4573a52cd44e0d2
harsha <ggharshabandi@gmail.com>
2022-02-12 17:17:12 -0600

    Pulling from main

--- frontend/src/components/navbar/inbox/InboxModalContainer.jsx
+++ frontend/src/components/navbar/inbox/InboxModalContainer.jsx
@@ -4,11 +4,13 @@ import InboxModal from "./InboxModal";
 
 const mSTP = state => ({
     currentUserId: state.session.user.id,
-
+    inbox: state.inbox,
 });
 
 const mDTP = dispatch => ({
-    fetchAllInboxes: userId => dispatch(fetchAllInboxes(userId))
+    fetchAllInboxes: userId => dispatch(fetchAllInboxes(userId)),
+    createInbox: inbox => dispatch(createInbox(inbox)),
+    updateInbox: inbox => dispatch(updateInbox(inbox))
 });
 
 export default connect(mSTP, mDTP)(InboxModal);
