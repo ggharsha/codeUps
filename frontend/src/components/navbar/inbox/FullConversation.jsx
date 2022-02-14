32e6b7c2dd1a5966d6ad18bbd7e5b8f51f4aee95
harsha <ggharshabandi@gmail.com>
2022-02-13 19:47:40 -0600

    Refactor inbox

--- frontend/src/components/navbar/inbox/FullConversation.jsx
+++ frontend/src/components/navbar/inbox/FullConversation.jsx
@@ -3,18 +3,23 @@ import React from 'react'
 export default class FullConversation extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {
+            senderId: this.props.currentUser.id,
+            receiverId: this.props.otherUser._id,
+            messages: ""
+        }
         this.handleSubmit = this.handleSubmit.bind(this);
         this.mergeConversations = this.mergeConversations.bind(this);
         this.merge = this.merge.bind(this);
     }
 
-    update(field) {
-
+    update() {
+        return e => this.setState({ ["messages"]: e.currentTarget.value });
     }
 
     handleSubmit(e) {
-
+        e.preventDefault();
+        this.props.updateInbox(this.state);
     }
 
     mergeConversations() {
