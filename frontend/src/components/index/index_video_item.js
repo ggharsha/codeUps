151a84eb550ec9527d21d7840d21a7c15b612cd4
Perry Xie <perry.xie1@gmail.com>
2022-02-21 10:47:32 -0800

    Add title to index videos

--- frontend/src/components/index/index_video_item.js
+++ frontend/src/components/index/index_video_item.js
@@ -2,6 +2,7 @@ import React from "react";
 
 const IndexVideoItem = ({ video }) => (
     <li className="video-index-item">
+        <h2 className='video-index-title'>{video.title}</h2>
         <video width="400" key={video._id} controls>
             <source src={video.videoUrl} type="video/mp4" />
             <source src={video.videoUrl} type="video/ogg" />
